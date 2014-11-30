45d34006cf129ae419dadda51a88c3a25441d7b6
Kamil Krupa <kamil.m.krupa@gmail.com>
2014-11-30 14:23:17 +0100

    greenhouse controller

---
+++ Pierwszy/src/edu/uz/issi/greenhouse/Event.java
@@ -0,0 +1,17 @@
+package edu.uz.issi.greenhouse;
+
+public abstract class Event {
+	  private long eventTime;
+	  protected final long delayTime;
+	  public Event(long delayTime) {
+	    this.delayTime = delayTime;
+	    start();
+	  }
+	  public void start() { // Allows restarting
+	    eventTime = System.currentTimeMillis() + delayTime;
+	  }
+	  public boolean ready() {
+	    return System.currentTimeMillis() >= eventTime;
+	  }
+	  public abstract void action();
+	} ///:~
